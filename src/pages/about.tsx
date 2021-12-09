08153eb2bc29429bf930122a0e92bd2066ae4b25
mu-suke <chelsea_08ym@yahoo.co.jp>
2021-12-09 17:37:57 +0900

    aboutページの実装

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@chakra-ui/react"
+import { NextPage } from "next"
+import React from "react"
+
+const AboutPage: NextPage = () => {
+  return <Heading>About Page!</Heading>
+}
+
+export default AboutPage
